becfd0420986dfc6c45cbb5c1b5d4e7546d682aa
zhou-l <31491660+zhou-l@users.noreply.github.com>
2022-11-10 01:00:42 +0800

    Add files via upload

--- frontend/src/components/ForceDirectedGraphView.vue
+++ frontend/src/components/ForceDirectedGraphView.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="ForceDirectedGraph">
     <h2>ForceDirectedGraph View</h2>
-    <svg width="1280" height="900"></svg>
+    <svg width="1280" height="900"><g/></svg>
     <h3>todo:plot rendering</h3>
     <!-- <p>获取到的多对比数据的节点为：{{multipleSearchValue[0].nodes}}</p> -->
     <!-- <p>获取到的多对比数据的边为：{{$store.state.multipleSearchValue.links}}</p> -->
     <!-- <p>获取到的多对比数据为：{{$store.state.multipleSearchValue}}</p> -->
     <p>获取到的多对比数据为：{{ multipleSearchValue }}</p>
-    <!-- <button @click="getMultipleOutcome">show</button> -->
+    <button @click="drawGraph">show</button>
     <!-- 缓存一个路由组件 -->
   </div>
 </template>
@@ -18,7 +18,7 @@
 import * as d3 from 'd3';
 import * as dagreD3 from 'dagre-d3';
 import bus from "../componentsInteraction/bus.js";
-import {mapState,mapGetters,mapMutations,mapActions} from 'vuex';
+import {mapState} from 'vuex';
 export default {
   name: "ForceDirectedGraph",
   data() {
@@ -29,11 +29,9 @@ export default {
   // mounted() {
 	// 		this.generateDag();
 	// 	},
-  mounted(){
-    // generateDag() {
-    // const data = d3.json("C:/MyProjects_yuxi/ComparativeVis/important_causalCompVis/PC_5_top_with_edges_nodes_3outcomes.json").then(function(data)
-    // const data = d3.json(multipleSearchValue).then(function(data)
-        const data = multipleSearchValue
+  methods:{
+    drawGraph(){
+    var  data = this.multipleSearchValue
     // {
         console.log(data); 
         var g = new dagreD3.graphlib.Graph().setGraph({});
@@ -51,10 +49,65 @@ export default {
             g.setEdge(edge.source, edge.target, {label: edge.value.toString()})});
         
         // Set some general styles
-        g.nodesList().forEach(function (v) {
-            var node = g.node(v);
-            node.rx = node.ry = 5;
+        // g.nodesList().forEach(function (v) {
+        //     var node = g.node(v);
+        //     node.rx = node.ry = 5;
+        // });
+
+        // Add some custom colors based on state
+        // g.node('death').style = "fill: #f77";
+        // g.node('multimorbidity_base').style = "fill: #7f7";
+        // g.node('physi_limit_base').style = "fill: #7f7";
+
+        var svg = d3.select("svg"),
+            inner = svg.select("g");
+
+        // Set up zoom support
+        var zoom = d3.zoom().on("zoom", function () {
+            inner.attr("transform", d3.event.transform);
         });
+        svg.call(zoom);
+
+        // Create the renderer
+        var render = new dagreD3.render();
+
+        // Run the renderer. This is what draws the final graph.
+        render(inner, g);
+
+        // Center the graph
+        var initialScale = 0.75;
+        svg.call(zoom.transform, d3.zoomIdentity.translate((svg.attr("width") - g.graph().width * initialScale) / 2, 20).scale(initialScale));
+
+        svg.attr('height', g.graph().height * initialScale + 40);
+        return data;
+    },
+  },
+  mounted(){
+    // generateDag() {
+    // const data = d3.json("C:/MyProjects_yuxi/ComparativeVis/important_causalCompVis/PC_5_top_with_edges_nodes_3outcomes.json").then(function(data)
+    // const data = d3.json(multipleSearchValue).then(function(data)
+       var data = this.multipleSearchValue;
+    // {
+        console.log(data); 
+        var g = new dagreD3.graphlib.Graph().setGraph({});
+
+        // Test with our 3 outcome graph(s)
+        var states = data.nodesList;
+
+        // Automatically label each of the nodes
+        states.forEach(function (state) { g.setNode(state.id, { label: state.id }); });
+
+        var edges = data.linksList;
+        edges.forEach(function(edge){
+            var valString = (edge.value*10).toString()+"px";
+            var styleString = "stroke-width: "+valString;
+            g.setEdge(edge.source, edge.target, {label: edge.value.toString()})});
+        
+        // Set some general styles
+        // g.nodesList().forEach(function (v) {
+        //     var node = g.node(v);
+        //     node.rx = node.ry = 5;
+        // });
 
         // Add some custom colors based on state
         // g.node('death').style = "fill: #f77";
@@ -87,9 +140,11 @@ export default {
 // );
   // }
   },
+
   computed:{
+  
     ...mapState(['multipleSearchValue'])
-  },
+  }
   
   // methods: {
   //   // getMultipleOutcome() {
